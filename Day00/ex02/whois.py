57fa480dd3002c28db2a9c6e8b5155fa1420c95a
bmellouk <badriamellouk@gmail.com>
2022-12-07 18:38:39 -0800

    Day00/ex02

---
+++ Day00/ex02/whois.py
@@ -0,0 +1,21 @@
+import sys
+
+if len(sys.argv) > 1 :
+
+    if len(sys.argv) != 2 :
+        print("AssertionError: more than one argument are provided")
+        exit()
+
+
+    num = sys.argv[1]; 
+
+    if num.isnumeric() == False :
+        print('AssertionError: argument is not integer')
+
+    else:
+            if int(num) == 0:
+                print("I'm Zero.") 
+            elif int(num)% 2 == 0:
+                print("I'm odd")
+            else:
+                print("I'm Even.")
